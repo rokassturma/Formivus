05bad15569dc4c27cd0f2d1751c3bfb20ce92cf1
Rokas <rokas.sturma@gmail.com>
2025-07-30 11:27:54 +0300

    updates

--- formivus-server/db/db.js
+++ formivus-server/db/db.js
@@ -1,10 +1,12 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'formivus_managing_system'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default db;
