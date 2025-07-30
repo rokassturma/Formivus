2c59beb1caf99e778dd4b961ce61c05bf2a6f34e
Rokas <rokas.sturma@gmail.com>
2025-07-31 02:11:33 +0300

    auth update

--- formivus-server/routes/auth.js
+++ formivus-server/routes/auth.js
@@ -1,24 +1,23 @@
-import express from 'express';
-import { login, register } from '../controllers/authController.js';
-import { verifyToken } from '../middleware/verifyToken.js';
+import express from "express";
+import { login, register } from "../controllers/authController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post('/login', login);
-router.post('/register', register);
+router.post("/login", login);
+router.post("/register", register);
 
-router.get('/me', verifyToken, (req, res) => {
-    res.status(200).json({ user: req.user });
+router.get("/me", verifyToken, (req, res) => {
+  res.status(200).json({ user: req.user });
 });
 
-router.post('/logout', (req, res) => {
-    res.clearCookie('access_token', {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'Lax'
-    });
-    res.status(200).json({ message: 'Logout successful' });
+router.post("/logout", (req, res) => {
+  res.clearCookie("access_token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  res.status(200).json({ message: "Logout successful" });
 });
-
 
 export default router;
